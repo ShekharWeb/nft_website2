fcdfd9827fbbb7eb1f7d5e9189e707d6a0cd3486
Shekhar <75965550+ShekharWeb@users.noreply.github.com>
2021-10-26 23:03:27 +0530

    Add files via upload

---
+++ precache-manifest.a44b388db5ee02653a6c005e1adec948.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "b20af3d53dd3aa1685f8",
+    "url": "/nft_website2/static/css/main.411a246e.chunk.css"
+  },
+  {
+    "revision": "b20af3d53dd3aa1685f8",
+    "url": "/nft_website2/static/js/main.b20af3d5.chunk.js"
+  },
+  {
+    "revision": "922ae2da033199993d78",
+    "url": "/nft_website2/static/css/1.a22af48b.chunk.css"
+  },
+  {
+    "revision": "922ae2da033199993d78",
+    "url": "/nft_website2/static/js/1.922ae2da.chunk.js"
+  },
+  {
+    "revision": "895658d797d2c60e8799",
+    "url": "/nft_website2/static/js/runtime~main.895658d7.js"
+  },
+  {
+    "revision": "e07b538aa51b6fa77f32828af21cb591",
+    "url": "/nft_website2/static/media/bootstrap-icons.e07b538a.woff2"
+  },
+  {
+    "revision": "7fb23f3b56a834d9dcf615b5e7c78da8",
+    "url": "/nft_website2/static/media/bootstrap-icons.7fb23f3b.woff"
+  },
+  {
+    "revision": "e96142c8261f0750ae097a922b8d3db9",
+    "url": "/nft_website2/static/media/hero2.e96142c8.jpg"
+  },
+  {
+    "revision": "07bd46fa856631a8b353e4ebc87cb67f",
+    "url": "/nft_website2/index.html"
+  }
+];
